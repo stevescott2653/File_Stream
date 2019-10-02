f99e7e1607beb56f7c31e45a4cdf1c58e71d6eca
Steven Scott <stevescott2653@gmail.com>
2019-10-02 11:54:29 -0400

    Add files via upload

---
+++ File_Stream/File_Stream/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace File_Stream
+{
+    public class FileStreamExample
+    {
+        public static void Main(string[] args)
+        {
+            FileStream f = new FileStream("e:\\b.txt", FileMode.OpenOrCreate); // creating file stream
+            f.WriteByte(65); // writing byte into stream
+            f.Close(); // closing stream
+        }
+    }
+}
